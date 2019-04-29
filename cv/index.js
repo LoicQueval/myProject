565cb6489f693c8fa4ea64b83ff9308b5380cbf9
LoicQueval <queval.loic@gmail.com>
2019-04-29 14:26:03 +0200

    replace left box

--- cv/index.js
+++ cv/index.js
@@ -22,20 +22,25 @@ findSelectorAndReplaceData('h1',me.lastName + ' ' + me.usualName + ' ' + me.firs
 findSelectorAndReplaceData('.intro', me.intro);
 
 
-findSelectorAndReplaceData('.diplomes','Diplômes');
+findSelectorAndReplaceData('#diplomes','Diplômes');
 
-findSelectorAndReplaceData('.diplome1', 'Bac Technicien Supérieur 2017');
-findSelectorAndReplaceData('.lycée1', 'Lycée Louis Armand à Nogent-sur-Marne');
-findSelectorAndReplaceData('.matière1', 'Systèmes numériques - option électronique et communications');
+findSelectorAndReplaceData('#diplome1', 'Bac Technicien Supérieur 2017');
+findSelectorAndReplaceData('#lycée1', 'Lycée Louis Armand à Nogent-sur-Marne');
+findSelectorAndReplaceData('#matière1', 'Systèmes numériques - option électronique et communications');
 
-findSelectorAndReplaceData('.diplome2', 'Baccalauréat Technique 2015');
-findSelectorAndReplaceData('.lycée2', 'Lycée Newton à Clichy-sur-Seine :');
-findSelectorAndReplaceData('.matière2','Sciences et technologies de l\'industrie et du développement durable - option Système d\'information numérique (mention assez bien)');
+findSelectorAndReplaceData('#diplome2', 'Baccalauréat Technique 2015');
+findSelectorAndReplaceData('#lycée2', 'Lycée Newton à Clichy-sur-Seine :');
+findSelectorAndReplaceData('#matière2','Sciences et technologies de l\'industrie et du développement durable - option Système d\'information numérique (mention assez bien)');
 
-findSelectorAndReplaceData('.expériencepro','Expérience professionnel');
+findSelectorAndReplaceData('#expériencepro','Expérience professionnel');
 
-findSelectorAndReplaceData('.date1','Septembre 2018 / Février 2019');
-findSelectorAndReplaceData('.entreprise1','Intérimaire chez Adecco');
+findSelectorAndReplaceData('#date1','Septembre 2018 / Février 2019');
+findSelectorAndReplaceData('#entreprise1','Intérimaire chez Adecco');
 
-findSelectorAndReplaceData('.date2', 'CDI Décembre 2017 / Avril 2018');
-findSelectorAndReplaceData('.entreprise2','Leclerc à Gennevilliers :');
+findSelectorAndReplaceData('#date2', 'CDI Décembre 2017 / Avril 2018');
+findSelectorAndReplaceData('#entreprise2','Leclerc à Gennevilliers :');
+findSelectorAndReplaceData('poste','Responsable du rayon liquide');
+
+findSelectorAndReplaceData('date3', 'Stage Mai / Juin 2017');
+findSelectorAndReplaceData('entreprise3','EANET à La Garenne Colombe :');
+findSelectorAndReplaceData('stage','Stage de 6 semaines dans une entreprise de site internet personnalisé à la demande');
