8c768d1e6d4e305a4646d620fa910776c7509357
LoicQueval <queval.loic@gmail.com>
2019-04-29 12:37:52 +0200

    Merge branch 'master' of C:\Users\WR2\WebstormProjects\myProject with conflicts.

--- cv/index.js
+++ cv/index.js
@@ -3,6 +3,8 @@ const me = {
     firstName: 'Loïc',
     lastName: 'Queval',
     usualName: 'Moreno',
+    intro: 'Connaissances en language informatique et en électronique',
+
     address: '1 rue Pierre Dac',
     cityCode: 92110,
     city: 'Clichy',
@@ -10,33 +12,30 @@ const me = {
     mail: 'queval.loic@gmail.com',
     birthDay: '11 juin 1997',
     gender: 'H',
-    studies: 'mes études',
 };
 
-const myName = document.querySelector('h1');
-myName.textContent = me.lastName + " " + me.usualName + " " + me.firstName;
-
-const info1 = document.querySelector('#infos1');
-info1.textContent = me.telephoneNumber;
-
-const info2 = document.querySelector('#infos2');
-info2.textContent = me.address + ' ' + me.cityCode + ' ' + me.city;
-
-const info3 = document.querySelector('#infos3');
-info3.textContent = me.mail;
-
-const info4 = document.querySelector('#infos4');
-info4.textContent = me.birthDay;
-
-const info5 = document.querySelector('#infos5');
-
-function sayGender(personParameter) {
-    if (personParameter.gender === 'H')
-        info5.textContent = 'Homme';
+function findSelectorAndReplaceData(selector, data) {
+    document.querySelector(selector).textContent = data
 }
 
-sayGender(me);
+findSelectorAndReplaceData('h1',me.lastName + ' ' + me.usualName + ' ' + me.firstName);
+findSelectorAndReplaceData('.intro', me.intro);
 
-const myDate = document.querySelector('#myDate');
-myDate.innerText = new Date().toLocaleDateString();
-console.log(myDate);
+
+findSelectorAndReplaceData('.diplomes','Diplômes');
+
+findSelectorAndReplaceData('.diplome1', 'Bac Technicien Supérieur 2017');
+findSelectorAndReplaceData('.lycée1', 'Lycée Louis Armand à Nogent-sur-Marne');
+findSelectorAndReplaceData('.matière1', 'Systèmes numériques - option électronique et communications');
+
+findSelectorAndReplaceData('.diplome2', 'Baccalauréat Technique 2015');
+findSelectorAndReplaceData('.lycée2', 'Lycée Newton à Clichy-sur-Seine :');
+findSelectorAndReplaceData('.matière2','Sciences et technologies de l\'industrie et du développement durable - option Système d\'information numérique (mention assez bien)');
+
+findSelectorAndReplaceData('.expériencepro','Expérience professionnel');
+
+findSelectorAndReplaceData('.date1','Septembre 2018 / Février 2019');
+findSelectorAndReplaceData('.entreprise1','Intérimaire chez Adecco');
+
+findSelectorAndReplaceData('.date2', 'CDI Décembre 2017 / Avril 2018');
+findSelectorAndReplaceData('.entreprise2','Leclerc à Gennevilliers :');
