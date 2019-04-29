2c739319843e3cf55e18a778c1e9934b82aa0986
LoicQueval <queval.loic@gmail.com>
2019-04-29 20:24:09 +0200

    END 2/2

--- cv/index.js
+++ cv/index.js
@@ -1,23 +1,9 @@
 import {me} from './data.js'
 
-
-console.log(document);
-
 me.forEach((value,key)=>{
     console.log(key,value, document.querySelector(key));
     document.querySelector(key).textContent = value;
 });
 
-
-
-/*ReplaceContenue('h1',me.lastName + ' ' + me.usualName + ' ' + me.firstName);
-ReplaceContenue('.intro', me.intro);
-
-function ReplaceContenue(selector, data) {
-    document.querySelector(selector).textContent = data
-}
-*/
-
 const today = document.querySelector('#actual-date');
 today.innerText = new Date().toLocaleDateString();
-
